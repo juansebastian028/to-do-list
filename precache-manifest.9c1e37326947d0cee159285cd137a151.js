d0d971fb45a5e438f3dd471749e56e1295ec51bb
Juan Sebastian <juansebastianvargasposada@gmail.com>
2020-12-29 19:22:56 -0500

    Updates

---
+++ precache-manifest.9c1e37326947d0cee159285cd137a151.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b3342ba096f73d3af9ced9b05503b9f",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "7daaa82f0764d902271d",
+    "url": "/to-do-list/static/js/2.b56e3130.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/to-do-list/static/js/2.b56e3130.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeb7dcf103c2b407062a",
+    "url": "/to-do-list/static/js/main.9c2ffb67.chunk.js"
+  },
+  {
+    "revision": "99620f4632993b275b31",
+    "url": "/to-do-list/static/js/runtime-main.65d94b1a.js"
+  }
+]);
